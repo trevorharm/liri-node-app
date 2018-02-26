bc9741eac9f252205c9a8f4548aa12b50f75e632
Trevor Harm <trevorharm@users.noreply.github.com>
2018-02-26 18:31:51 -0500

    8

--- liri.js
+++ liri.js
@@ -44,7 +44,15 @@ function twit(){
 };
 
 // Spotify function
-
+function spot() {
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data); 
+      });
+}
 
 
 // OMDB function
