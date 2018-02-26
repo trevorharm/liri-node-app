92b0775ac75a9cfd3139e889125621640af7ad26
Trevor Harm <trevorharm@users.noreply.github.com>
2018-02-26 10:46:06 -0500

    5

--- liri.js
+++ liri.js
@@ -1,6 +1,9 @@
 // node dependencies defined here
 require("dotenv").config();
 var keys = require("./keys.js");
+var Twitter = require('twitter');
+var request = require("request");
+var Spotify = require('node-spotify-api');
 
 // define global variables
 var spotify = new Spotify(keys.spotify);
